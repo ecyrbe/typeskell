8b307270b0dd8a8300b32f4d57bea801edb4ac82
ecyrbe <ecyrbe@gmail.com>
2023-12-30 22:09:49 +0100

    feat: add more tuples utility types

--- src/utils/tuples.ts
+++ src/utils/tuples.ts
@@ -1,3 +1,15 @@
+import { Kind, $ } from "@kinds";
+
+export type Tuple<
+  T,
+  N extends number,
+  $acc extends unknown[] = [],
+> = $acc["length"] extends N ? $acc : Tuple<T, N, [...$acc, T]>;
+
+export type Tail<T extends unknown[]> = T extends [unknown, ...infer R]
+  ? R
+  : [];
+
 export type SplitAt<
   N extends number,
   T extends unknown[],
@@ -7,3 +19,11 @@ export type SplitAt<
   : T extends [infer H, ...infer R]
     ? SplitAt<N, R, [...$acc, H]>
     : [$acc, []];
+
+type MapImpl<T, Fn extends Kind> = {
+  [K in keyof T]: $<Fn, [T[K]]>;
+};
+
+export interface Map<Fn extends Kind> extends Kind {
+  return: MapImpl<this["rawArgs"], Fn>;
+}
