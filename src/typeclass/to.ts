1e2beaa33ee58307958333a1ee41d19e6aefe6bc
ecyrbe <ecyrbe@gmail.com>
2023-12-31 14:24:39 +0100

    Improve To Typeclass

--- src/typeclass/to.ts
+++ src/typeclass/to.ts
@@ -1,28 +1,31 @@
 import type { Kind, $ } from "@kinds";
 import { GenericFn } from "@utils/functions";
-import { Dec } from "@utils/numbers";
+import { Inc } from "@utils/numbers";
 
 export interface ToParams extends Kind {
-  return: this["rawArgs"] extends [infer A, ...infer Args]
-    ? [f: (...args: Args) => A]
+  return: this["rawArgs"] extends [infer B, any, ...infer Args]
+    ? [f: (...args: Args) => B]
     : never;
 }
 
 export interface ToResult<F extends Kind> extends Kind {
-  return: this["rawArgs"] extends [infer A, ...infer Args]
-    ? (fa: $<F, [A, ...Args]>) => A
+  return: this["rawArgs"] extends [infer B, infer A, ...infer Args]
+    ? (fa: $<F, [A, ...Args]>) => A | B
     : never;
 }
 
+/**
+ * To is a typeclass that provides a way to extract a value from a type.
+ */
 export interface To<F extends Kind> {
   /**
-   * to :: f (e -> a) -> F a -> a
+   * to :: f (e -> b) -> F a -> a | b
    *
-   * to :: <A,...E>(f: (...args: E)=> A) => (fa: $<F, [A,...E]>) => A
+   * to :: <A, B,...E>(f: (...args: E)=> B) => (fa: $<F, [A,...E]>) => A | B
    *
    * @param f : f (e -> a)
    * @returns F a -> a
    *
    */
-  getOrElse: GenericFn<F["arity"], ToParams, ToResult<F>>;
+  getOrElse: GenericFn<Inc<F["arity"]>, ToParams, ToResult<F>>;
 }
