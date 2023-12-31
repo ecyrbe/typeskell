d78d5dc53236cc93219576253eaad6c6be9b0d64
ecyrbe <ecyrbe@gmail.com>
2023-12-31 14:24:10 +0100

    add typeclass docs

--- src/typeclass/of.ts
+++ src/typeclass/of.ts
@@ -2,6 +2,9 @@ import type { Kind, $ } from "@kinds";
 import { ToDefaultParam } from "../kinds/defaults";
 import { Tail } from "../utils/tuples";
 
+/**
+ * Of is a typeclass that provides a way to inject a value into a type.
+ */
 export interface Of<F extends Kind> {
   of: <A>(a: A) => $<F, [A, ...Tail<ToDefaultParam<F["signature"]>>]>;
 }
