5beaa1016a3101f9f61cc911f40e46a71473c2db
ecyrbe <ecyrbe@gmail.com>
2024-01-06 17:53:43 +0100

    fix(groups): add exports

--- src/data/boolean.ts
+++ src/data/boolean.ts
@@ -1,12 +1,12 @@
 import * as groups from '@typeclass/groups';
 
-const GroupOr: groups.Group<boolean> = {
+export const GroupOr: groups.Group<boolean> = {
   concat: (a, b) => a || b,
   invert: a => !a,
   identity: false,
 };
 
-const GroupAnd: groups.Group<boolean> = {
+export const GroupAnd: groups.Group<boolean> = {
   concat: (a, b) => a && b,
   invert: a => !a,
   identity: true,
--- src/data/number.ts
+++ src/data/number.ts
@@ -1,20 +1,20 @@
 import * as groups from '@typeclass/groups';
 
-const SemiGroupMax: groups.SemiGroup<number> = {
+export const SemiGroupMax: groups.SemiGroup<number> = {
   concat: (a, b) => Math.max(a, b),
 };
 
-const SemiGroupMin: groups.SemiGroup<number> = {
+export const SemiGroupMin: groups.SemiGroup<number> = {
   concat: (a, b) => Math.min(a, b),
 };
 
-const GroupSum: groups.Group<number> = {
+export const GroupSum: groups.Group<number> = {
   concat: (a, b) => a + b,
   invert: a => -a,
   identity: 0,
 };
 
-const GroupProduct: groups.Group<number> = {
+export const GroupProduct: groups.Group<number> = {
   concat: (a, b) => a * b,
   invert: a => 1 / a,
   identity: 1,
