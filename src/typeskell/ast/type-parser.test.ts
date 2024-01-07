4c4db981e9db31e2409d8df9ab37f5e3bb34cadb
ecyrbe <ecyrbe@gmail.com>
2024-01-08 00:04:12 +0100

    feat(typeskell): implement ast parser at type level

--- src/typeskell/ast/type-parser.test.ts
+++ src/typeskell/ast/type-parser.test.ts
@@ -1,5 +1,6 @@
 import { describe, it, expectTypeOf } from 'vitest';
-import type { TrimParens, Trim, SplitFnWithParens, SplitParamWithParens } from './type-parser';
+import type { TrimParens, Trim, SplitFnWithParens, SplitParamWithParens, ParseAST } from './type-parser';
+import { Expect, Equal } from 'type-testing';
 
 describe('Type parser', () => {
   it('should trim parens', () => {
@@ -39,5 +40,148 @@ describe('Type parser', () => {
     expectTypeOf<SplitParamWithParens<'a b c'>>().toEqualTypeOf<['a', 'b', 'c']>();
     expectTypeOf<SplitParamWithParens<'a (b -> c)'>>().toEqualTypeOf<['a', '(b -> c)']>();
     expectTypeOf<SplitParamWithParens<'a (b -> c) d'>>().toEqualTypeOf<['a', '(b -> c)', 'd']>();
+  });
+
+  it('should parse AST', () => {
+    expectTypeOf<ParseAST<'a'>>().toEqualTypeOf<{ type: 'type'; name: 'a' }>();
+    expectTypeOf<ParseAST<'a b -> (c -> d)'>>().toEqualTypeOf<{
+      type: 'function';
+      args: [{ type: 'type'; name: 'a' }, { type: 'type'; name: 'b' }];
+      result: { type: 'function'; args: [{ type: 'type'; name: 'c' }]; result: { type: 'type'; name: 'd' } };
+    }>();
+    expectTypeOf<ParseAST<'a b -> (c d -> e)'>>().toEqualTypeOf<{
+      type: 'function';
+      args: [{ type: 'type'; name: 'a' }, { type: 'type'; name: 'b' }];
+      result: {
+        type: 'function';
+        args: [{ type: 'type'; name: 'c' }, { type: 'type'; name: 'd' }];
+        result: { type: 'type'; name: 'e' };
+      };
+    }>();
+    expectTypeOf<ParseAST<'a b -> c'>>().toEqualTypeOf<{
+      type: 'function';
+      args: [{ type: 'type'; name: 'a' }, { type: 'type'; name: 'b' }];
+      result: { type: 'type'; name: 'c' };
+    }>();
+    expectTypeOf<ParseAST<'a b -> c'>>().toEqualTypeOf<{
+      type: 'function';
+      args: [{ type: 'type'; name: 'a' }, { type: 'type'; name: 'b' }];
+      result: { type: 'type'; name: 'c' };
+    }>();
+    expectTypeOf<ParseAST<'(F a b -> b) -> a -> (c -> d)'>>().toEqualTypeOf<{
+      type: 'chain';
+      args: [
+        {
+          type: 'function';
+          args: [
+            {
+              type: 'typeconstructor';
+              name: 'F';
+              params: [{ type: 'type'; name: 'a' }, { type: 'type'; name: 'b' }];
+            },
+          ];
+          result: { type: 'type'; name: 'b' };
+        },
+      ];
+      result: {
+        type: 'function';
+        args: [{ type: 'type'; name: 'a' }];
+        result: { type: 'function'; args: [{ type: 'type'; name: 'c' }]; result: { type: 'type'; name: 'd' } };
+      };
+    }>();
+    expectTypeOf<ParseAST<'(F a b -> b) -> a -> c -> d'>>().toEqualTypeOf<{
+      type: 'chain';
+      args: [
+        {
+          type: 'function';
+          args: [
+            {
+              type: 'typeconstructor';
+              name: 'F';
+              params: [{ type: 'type'; name: 'a' }, { type: 'type'; name: 'b' }];
+            },
+          ];
+          result: { type: 'type'; name: 'b' };
+        },
+      ];
+      result: {
+        type: 'chain';
+        args: [{ type: 'type'; name: 'a' }];
+        result: { type: 'function'; args: [{ type: 'type'; name: 'c' }]; result: { type: 'type'; name: 'd' } };
+      };
+    }>();
+    expectTypeOf<ParseAST<'F (F a) -> F a'>>().toEqualTypeOf<{
+      type: 'function';
+      args: [
+        {
+          type: 'typeconstructor';
+          name: 'F';
+          params: [
+            {
+              type: 'typeconstructor';
+              name: 'F';
+              params: [{ type: 'type'; name: 'a' }];
+            },
+          ];
+        },
+      ];
+      result: {
+        type: 'typeconstructor';
+        name: 'F';
+        params: [{ type: 'type'; name: 'a' }];
+      };
+    }>();
+    expectTypeOf<ParseAST<'(a -> b) -> F (G a) -> F (G b)'>>().toEqualTypeOf<{
+      type: 'chain';
+      args: [
+        {
+          type: 'function';
+          args: [{ type: 'type'; name: 'a' }];
+          result: { type: 'type'; name: 'b' };
+        },
+      ];
+      result: {
+        type: 'function';
+        args: [
+          {
+            type: 'typeconstructor';
+            name: 'F';
+            params: [
+              {
+                type: 'typeconstructor';
+                name: 'G';
+                params: [{ type: 'type'; name: 'a' }];
+              },
+            ];
+          },
+        ];
+        result: {
+          type: 'typeconstructor';
+          name: 'F';
+          params: [
+            {
+              type: 'typeconstructor';
+              name: 'G';
+              params: [{ type: 'type'; name: 'b' }];
+            },
+          ];
+        };
+      };
+    }>();
+    expectTypeOf<ParseAST<'(a -> F b ..β) -> F a ..α -> F b ..αβ'>>().toEqualTypeOf<{
+      type: 'chain';
+      args: [
+        {
+          type: 'function';
+          args: [{ type: 'type'; name: 'a' }];
+          result: { type: 'typeconstructor'; name: 'F'; params: [{ type: 'type'; name: 'b' }]; spread: 'β' };
+        },
+      ];
+      result: {
+        type: 'function';
+        args: [{ type: 'typeconstructor'; name: 'F'; params: [{ type: 'type'; name: 'a' }]; spread: 'α' }];
+        result: { type: 'typeconstructor'; name: 'F'; params: [{ type: 'type'; name: 'b' }]; spread: 'αβ' };
+      };
+    }>();
   });
 });
--- src/typeskell/ast/type-parser.ts
+++ src/typeskell/ast/type-parser.ts
@@ -1,7 +1,8 @@
 import { Inc, Dec } from '@utils/numbers';
 import { Tail } from '@utils/tuples';
+import { GreekChars, LowercaseChars, UppercaseChars } from './types';
 
-export type TrimParens<T extends string> = T extends `(${infer U})` ? U : T;
+export type TrimParens<T> = T extends `(${infer U})` ? U : T;
 
 type StringToTuple<T extends string, $acc extends unknown[] = []> = T extends `${infer A}${infer B}`
   ? StringToTuple<B, [...$acc, A]>
@@ -28,7 +29,7 @@ type SplitTupleFnWithParens<
         : SplitTupleFnWithParens<Rest, $accTuple, `${$acc}${Char}`, $nested>
   : [...$accTuple, Trim<$acc>];
 
-export type SplitFnWithParens<Input extends string> = SplitTupleFnWithParens<StringToTuple<Input>>;
+export type SplitFnWithParens<Input> = Input extends string ? SplitTupleFnWithParens<StringToTuple<Input>> : never;
 
 type SplitTupleParamWithParens<
   T extends string[],
@@ -51,4 +52,63 @@ type SplitTupleParamWithParens<
     ? $accTuple
     : [...$accTuple, Trim<$acc>];
 
-export type SplitParamWithParens<Input extends string> = SplitTupleParamWithParens<StringToTuple<Input>>;
+export type SplitParamWithParens<Input> = Input extends string
+  ? SplitTupleParamWithParens<StringToTuple<Input>>
+  : never;
+
+export type MapArgsAST<Input> = {
+  [K in keyof Input]: ParseAST<TrimParens<Input[K]>>;
+};
+
+export type ParseArgsAST<Input extends string> = Input extends `${UppercaseChars}${string}`
+  ? [ParseTypeConstructorAST<Input>]
+  : MapArgsAST<SplitParamWithParens<Input>>;
+
+export type ParseTypeConstructorAST<Input, $args = SplitParamWithParens<Input>> = $args extends [
+  infer Constructor,
+  ...infer Args,
+  infer Last,
+]
+  ? Last extends `..${infer Spread}`
+    ? {
+        type: 'typeconstructor';
+        name: Constructor;
+        params: MapArgsAST<Args>;
+        spread: Spread;
+      }
+    : {
+        type: 'typeconstructor';
+        name: Constructor;
+        params: MapArgsAST<[...Args, Last]>;
+      }
+  : never;
+
+export type ParseFromArrayToManyAST<Types> = Types extends [infer Type extends string, ...infer Rest]
+  ? Rest['length'] extends 0
+    ? Type extends `${UppercaseChars}${string}`
+      ? [ParseTypeConstructorAST<Type>]
+      : Type extends `${LowercaseChars}${string}`
+        ? [{ type: 'type'; name: Type }]
+        : ParseArgsAST<Type>
+    : Rest['length'] extends 1
+      ? [
+          {
+            type: 'function';
+            args: ParseArgsAST<Type>;
+            result: ParseAST<TrimParens<Rest[0]>>;
+          },
+        ]
+      : [
+          {
+            type: 'chain';
+            args: ParseArgsAST<Type>;
+            result: ParseFromArrayToManyAST<Rest>[0];
+          },
+        ]
+  : never;
+
+export type ParseToManyAST<Input, $result = ParseFromArrayToManyAST<SplitFnWithParens<Input>>> = $result;
+
+export type ParseAST<Input, $result = ParseToManyAST<Input>> = $result extends [infer Result, ...any[]]
+  ? Result
+  : never;
