a55be039b896501683e3c26b61c4aab2767542d2
ecyrbe <ecyrbe@gmail.com>
2024-01-07 18:07:33 +0100

    feat(haskell): add tokenisation

--- src/utils/functions.test.ts
+++ src/utils/functions.test.ts
@@ -1,4 +1,6 @@
 import { describe, expectTypeOf, expect, it } from 'vitest';
+import { Inc, Dec } from './numbers';
+import { Tail } from './tuples';
 
 // parse a function in haskell style and generate a generic function in typescript style
 // lawer case a, b, c, d, e are generic types and F, G, H are generic type constructors
@@ -20,10 +22,62 @@ import { describe, expectTypeOf, expect, it } from 'vitest';
 // compose: parseHaskellFn('(a -> b) -> F (G a ..α) ..β -> F (G b ..α) ..β', {F: 2}) // '<A1, A2>(f: (a: A1) => A2) => <B1, B2>(fga: F<G<A1, B1>, B2>) => F<G<A2, B1>, B2>'
 // ```
 
+type TrimParens<T extends string> = T extends `(${infer U})` ? U : T;
+
 function trimParens(input: string): string {
   if (input[0] !== '(' || input[input.length - 1] !== ')') return input;
   return input.slice(1, -1).trim();
 }
+
+type StringToTuple<T extends string, $acc extends unknown[] = []> = T extends `${infer A}${infer B}`
+  ? StringToTuple<B, [...$acc, A]>
+  : $acc;
+
+type TrimLeft<T extends string> = T extends ` ${infer U}` ? TrimLeft<U> : T;
+type TrimRight<T extends string> = T extends `${infer U} ` ? TrimRight<U> : T;
+type Trim<T extends string> = TrimLeft<TrimRight<T>>;
+
+type SplitTupleFnWithParens<
+  T extends string[],
+  $accTuple extends unknown[] = [],
+  $acc extends string = '',
+  $nested extends number = 0,
+> = T extends [infer Char extends string, ...infer Rest extends string[]]
+  ? Char extends `(`
+    ? SplitTupleFnWithParens<Rest, $accTuple, `${$acc}${Char}`, Inc<$nested>>
+    : Char extends `)`
+      ? SplitTupleFnWithParens<Rest, $accTuple, `${$acc}${Char}`, Dec<$nested>>
+      : $nested extends 0
+        ? [Char, Rest[0]] extends ['-', '>']
+          ? SplitTupleFnWithParens<Tail<Rest>, [...$accTuple, Trim<$acc>], '', 0>
+          : SplitTupleFnWithParens<Rest, $accTuple, `${$acc}${Char}`, 0>
+        : SplitTupleFnWithParens<Rest, $accTuple, `${$acc}${Char}`, $nested>
+  : [...$accTuple, Trim<$acc>];
+
+type SplitFnWithParens<Input extends string> = SplitTupleFnWithParens<StringToTuple<Input>>;
+
+type SplitTupleParamWithParens<
+  T extends string[],
+  $accTuple extends unknown[] = [],
+  $acc extends string = '',
+  $nested extends number = 0,
+> = T extends [infer Char extends string, ...infer Rest extends string[]]
+  ? Char extends `(`
+    ? SplitTupleParamWithParens<Rest, $accTuple, `${$acc}${Char}`, Inc<$nested>>
+    : Char extends `)`
+      ? SplitTupleParamWithParens<Rest, $accTuple, `${$acc}${Char}`, Dec<$nested>>
+      : $nested extends 0
+        ? Char extends ' '
+          ? $acc extends ''
+            ? SplitTupleParamWithParens<Rest, $accTuple>
+            : SplitTupleParamWithParens<Rest, [...$accTuple, Trim<$acc>], '', 0>
+          : SplitTupleParamWithParens<Rest, $accTuple, `${$acc}${Char}`, 0>
+        : SplitTupleParamWithParens<Rest, $accTuple, `${$acc}${Char}`, $nested>
+  : Trim<$acc> extends ''
+    ? $accTuple
+    : [...$accTuple, Trim<$acc>];
+
+type SplitParamWithParens<Input extends string> = SplitTupleParamWithParens<StringToTuple<Input>>;
 
 function splitWithParens(input: string, split: string): string[] {
   let parentheses = 0;
