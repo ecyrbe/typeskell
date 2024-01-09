352446fcedd14cdcaad7cc880e3f3390d6e58458
ecyrbe <ecyrbe@gmail.com>
2024-01-09 23:02:46 +0100

    feat(build): add build

--- vitest.config.js
+++ vitest.config.js
@@ -1,12 +1,13 @@
-import path from "path";
+import path from 'path';
 
 export default {
   resolve: {
     alias: {
-      "@kinds": path.resolve(__dirname, "./src/kinds"),
-      "@utils": path.resolve(__dirname, "./src/utils"),
-      "@typeclass": path.resolve(__dirname, "./src/typeclass"),
-      "@data": path.resolve(__dirname, "./src/data"),
+      '@kinds': path.resolve(__dirname, './src/kinds'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@typeclass': path.resolve(__dirname, './src/typeclass'),
+      '@data': path.resolve(__dirname, './src/data'),
+      '@typeskell': path.resolve(__dirname, './src/typeskell'),
     },
   },
 };
