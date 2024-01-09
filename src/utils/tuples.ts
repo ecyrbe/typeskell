39b967473338c929b57c7b0e150f88e4db3866a4
ecyrbe <ecyrbe@gmail.com>
2024-01-09 01:21:27 +0100

    feat: add more tuple helpers

--- src/utils/tuples.ts
+++ src/utils/tuples.ts
@@ -4,6 +4,14 @@ export type Tuple<T, N extends number, $acc extends unknown[] = []> = $acc['leng
   ? $acc
   : Tuple<T, N, [...$acc, T]>;
 
+export type StringToTuple<T extends string, $acc extends unknown[] = []> = T extends `${infer A}${infer B}`
+  ? StringToTuple<B, [...$acc, A]>
+  : $acc;
+
+export type Range<Max extends number, $acc extends unknown[] = []> = $acc['length'] extends Max
+  ? $acc
+  : Range<Max, [...$acc, $acc['length']]>;
+
 export type Tail<T extends unknown[]> = T extends [unknown, ...infer R] ? R : [];
 
 export type SplitAt<N extends number, T extends unknown[], $acc extends unknown[] = []> = $acc['length'] extends N
@@ -15,6 +23,10 @@ export type SplitAt<N extends number, T extends unknown[], $acc extends unknown[
 type MapImpl<T, Fn extends Kind> = {
   [K in keyof T]: $<Fn, [T[K]]>;
 };
+
+export type Flatten<T, $acc extends unknown[] = []> = T extends [infer Head extends unknown[], ...infer Tail]
+  ? Flatten<Tail, [...$acc, ...Head]>
+  : $acc;
 
 export interface Map<Fn extends Kind> extends Kind {
   return: MapImpl<this['rawArgs'], Fn>;
