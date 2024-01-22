a323824360ab497d383c7ff792c9cc7aa9bafaf7
ecyrbe <ecyrbe@gmail.com>
2024-01-22 22:32:56 +0100

    chore(monad): impl flatten with inference

--- src/typeclass/monad/monad.impl.ts
+++ src/typeclass/monad/monad.impl.ts
@@ -1,7 +1,9 @@
 import { Kind, $ } from '@kinds';
 import { Monad } from './monad';
 import { identity } from '@utils/functions';
+import { pipe } from '../../pipe';
 
-export const flatten = (
-  m: Monad<Kind.F2>,
-): (<A, E1, E2>(mma: $<Kind.F2, [$<Kind.F2, [A, E1]>, E2]>) => $<Kind.F2, [A, E1 | E2]>) => m.flatMap(identity);
+export const flatten =
+  (m: Monad<Kind.F2>) =>
+  <A, E1, E2>(mma: $<Kind.F2, [$<Kind.F2, [A, E1]>, E2]>) =>
+    pipe(mma, m.flatMap(identity));
