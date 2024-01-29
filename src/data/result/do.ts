cba6ed8ed5265c4d2c3545d8a4df713a579896f5
ecyrbe <ecyrbe@gmail.com>
2024-01-29 19:50:51 +0100

    feat(result): add do notation

---
+++ src/data/result/do.ts
@@ -0,0 +1,41 @@
+import { Functor, Monad, Result } from './result';
+import * as tFunctor from '@typeclass/functor';
+import * as tMonad from '@typeclass/monad';
+
+/**
+ * Do notation for Option
+ * it allows you to chain multiple Option computations together
+ * and not worry about the None case
+ *
+ * @example
+ * ```ts
+ * pipe(
+ *  Do,
+ *  bind('a', () => of(1)),
+ *  bind('b', ({ a }) => of(a + 1)),
+ *  map(({ a, b }) => a + b),
+ * )
+ * ```
+ */
+export const Do = Monad.of({});
+
+/**
+ * bind an option to a name
+ * @param name name of the option
+ * @param f function that returns an option
+ * @returns Option with the name binded
+ *
+ * @example
+ * ```ts
+ * pipe(
+ *  Do,
+ *  bind('a', () => of(1)),
+ *  bind('b', ({ a }) => of(a + 1)),
+ *  map(({ a, b }) => a + b),
+ * )
+ * ```
+ */
+export const bind = tMonad.bind(Monad);
+
+const $let = tFunctor.let(Functor);
+export { $let as let };
