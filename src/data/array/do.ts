eb8ed27216e541047045446a5ccd2333c22facf4
ecyrbe <ecyrbe@gmail.com>
2024-01-29 14:01:35 +0100

    feat(array): add do notation

---
+++ src/data/array/do.ts
@@ -0,0 +1,42 @@
+import { of, Monad, Functor } from './array';
+import * as tFunctor from '@typeclass/functor';
+import * as tMonad from '@typeclass/monad';
+
+/**
+ * Do notation for Option
+ * it allows you to chain multiple Option computations together
+ * and not worry about the None case
+ *
+ * @example
+ * ```ts
+ * pipe(
+ *  Do,
+ *  bind('a', () => some(1)),
+ *  bind('b', ({ a }) => some(a + 1)),
+ *  map(({ a, b }) => a + b),
+ * )
+ * ```
+ */
+export const Do: Array<{}> = of({});
+
+/**
+ * bind an option to a name
+ * @param name name of the option
+ * @param f function that returns an option
+ * @returns Option with the name binded
+ *
+ * @example
+ * ```ts
+ * pipe(
+ *  Do,
+ *  bind('a', () => some(1)),
+ *  bind('b', ({ a }) => some(a + 1)),
+ *  map(({ a, b }) => a + b),
+ * )
+ * ```
+ */
+export const bind = tMonad.bind(Monad);
+
+const $let = tFunctor.let(Functor);
+
+export { $let as let };
