1ff1689eefebf6ce635a10613628c9aeda0fd1f1
ecyrbe <ecyrbe@gmail.com>
2024-02-01 19:00:36 +0100

    feat(data): add iooption

---
+++ src/data/iooption/do.ts
@@ -0,0 +1,40 @@
+import { Functor, Monad } from './iooption';
+import * as tFunctor from '@typeclass/functor';
+import * as tMonad from '@typeclass/monad';
+
+/**
+ * Do notation for IOOption
+ * it allows you to chain multiple IOOption computations together
+ *
+ * @example
+ * ```ts
+ * pipe(
+ *  Do,
+ *  bind('a', () => of(1)),
+ *  bind('b', ({ a }) => of(a + 1)),
+ *  map(({ a, b }) => a + b),
+ * )
+ * ```
+ */
+export const Do = Monad.of({});
+
+/**
+ * bind an IOOption to a name
+ * @param name name of the IOOption
+ * @param f function that returns a IOOption
+ * @returns IOOption with the name binded
+ *
+ * @example
+ * ```ts
+ * pipe(
+ *  Do,
+ *  bind('a', () => of(1)),
+ *  bind('b', ({ a }) => of(a + 1)),
+ *  map(({ a, b }) => a + b),
+ * )
+ * ```
+ */
+export const bind = tMonad.bind(Monad);
+
+const $let = tFunctor.let(Functor);
+export { $let as let };
---
+++ src/data/iooption/iooption.ts
@@ -0,0 +1,89 @@
+import { Kind } from '@kinds';
+import * as O from '@data/option';
+import * as I from '@data/io';
+import * as tfunctor from '@typeclass/functor';
+import * as tOf from '@typeclass/of';
+import * as tTo from '@typeclass/to';
+import * as tApplicative from '@typeclass/applicative';
+import * as tMonad from '@typeclass/monad';
+import * as tFoldable from '@typeclass/foldable';
+import * as tSemiAlternative from '@typeclass/semialternative';
+import { pipe } from '@utils/pipe';
+
+export interface IOOption<A> extends I.IO<O.Option<A>> {}
+
+export interface TIOOption extends Kind.unary {
+  return: IOOption<this['arg0']>;
+}
+
+export const none: <A = unknown>() => IOOption<A> = () => I.of(O.none());
+
+export const some: <A>(a: A) => IOOption<A> = a => I.of(O.some(a));
+
+export const Of: tOf.Of<TIOOption> = {
+  of: some,
+};
+
+export const Functor: tfunctor.Functor<TIOOption> = {
+  map: f => io => pipe(io, I.map(O.map(f))),
+};
+
+export const To: tTo.To<TIOOption> = {
+  getOrElse: f => io => pipe(io(), O.getOrElse(f)),
+};
+
+export const Applicative: tApplicative.Applicative<TIOOption> = {
+  ...Of,
+  ...Functor,
+  ap: fa => fab => pipe(fab, I.map(O.ap(fa()))),
+};
+
+export const Monad: tMonad.Monad<TIOOption> = {
+  ...Applicative,
+  flatMap: f => fa => {
+    const result = pipe(fa(), O.map(f));
+    if (O.isNone(result)) return none();
+    return result.value;
+  },
+};
+
+export const Foldable: tFoldable.Foldable<TIOOption> = {
+  reduce: (f, b) => fa => pipe(fa(), O.reduce(f, b)),
+};
+
+export const SemiAlternative: tSemiAlternative.SemiAlternative<TIOOption> = {
+  ...Functor,
+  or: fb => fa => pipe(fa, I.map(O.or(fb()))),
+};
+
+export const of = Of.of;
+
+export const getOrElse = To.getOrElse;
+
+export const getOr = tTo.getOr(To);
+
+export const map = Functor.map;
+
+export const mapCompose = tfunctor.mapCompose(Functor, Functor);
+
+export const flap = tfunctor.flap(Functor);
+
+export const as = tfunctor.as(Functor);
+
+export const ap = Applicative.ap;
+
+export const liftA2 = tApplicative.liftA2(Applicative);
+
+export const product = tApplicative.product(Applicative);
+
+export const productMany = tApplicative.productMany(Applicative);
+
+export const flatMap = Monad.flatMap;
+
+export const chain = flatMap;
+
+export const flatten = tMonad.flatten(Monad);
+
+export const reduce = Foldable.reduce;
+
+export const or = SemiAlternative.or;
